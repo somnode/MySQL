4ea7194a00632a538d87b7030791c5ecfbe9a48c
root <root@my.lxd>
2019-08-09 10:44:25 +0000

    gcc/g++ installation
    when using ubuntu 18.04, should check that gcc/g++ are installed with the correct version (5).

--- mysql_install.sh
+++ mysql_install.sh
@@ -1,5 +1,20 @@
 sudo apt-get install libmysqlclient20 libmysqlclient-dev
 sudo apt-get install gcc g++
+
+# if using ubuntu 18.04, should reinstall gcc/g++ to gcc/g++ 5
+# install gcc, g++
+sudo apt install g++-5
+sudo apt install gcc-5
+# change the symlink to point to gcc 5 and g++ 5
+sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-5 10
+sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-5 20
+sudo update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-5 10
+sudo update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-5 20
+sudo update-alternatives --install /usr/bin/cc cc /usr/bin/gcc 30
+sudo update-alternatives --set cc /usr/bin/gcc
+sudo update-alternatives --install /usr/bin/c++ c++ /usr/bin/g++ 30
+sudo update-alternatives --set c++ /usr/bin/g++
+
 sudo apt-get install fakeroot build-essential ncurses-dev xz-utils libssl-dev bc
 
 tar -xvf mysql-5.6.14.tar.gz
